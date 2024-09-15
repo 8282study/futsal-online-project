3826a2170f5dc3d47692cdfabe863fc269a71438
tjdcjf1996 <r_louis@naver.com>
2024-09-15 15:44:08 +0900

    Bearer 구분 기준 변경
    URL 인코딩 없이 공백으로 구분 처리

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -11,16 +11,18 @@ export default async function (req, res, next) {
         if (!authorization)
             throw new Error('요청한 사용자의 토큰이 존재하지 않습니다.');
 
-        const [tokenType, token] = authorization.split('%20');
+        const [tokenType, token] = authorization.split(' ');
         console.log(tokenType, token);
         if (tokenType !== 'Bearer')
             throw new Error('토큰타입이 Bearer형식이 아닙니다.');
 
         const decodedToken = jwt.verify(token, SECRET_CODE);
+        if (!decodedToken) throw new Error('토큰이 정상적이지 않습니다.');
+
         const userID = decodedToken.userID;
 
         const user = await prisma.users.findFirst({
-            where: { userID: +userID },
+            where: { userID: userID },
         });
         if (!user) throw new Error('토큰 사용자가 존재하지 않습니다.');
 
