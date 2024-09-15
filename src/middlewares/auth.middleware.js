fe4a19de82d5ea4415804867582ca0a3e9a52fc1
tjdcjf1996 <r_louis@naver.com>
2024-09-15 20:42:33 +0900

    예외처리 추가
    토큰 관련 예외처리 추가

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -7,19 +7,25 @@ const SECRET_CODE = process.env.SECRET_CODE;
 export default async function (req, res, next) {
     try {
         const authorization = req.headers['authorization'];
-        console.log('auth2 : ' + authorization);
         if (!authorization)
             throw new Error('요청한 사용자의 토큰이 존재하지 않습니다.');
 
         const [tokenType, token] = authorization.split(' ');
-        console.log(tokenType, token);
+
         if (tokenType !== 'Bearer')
             throw new Error('토큰타입이 Bearer형식이 아닙니다.');
 
-        const decodedToken = jwt.verify(token, SECRET_CODE);
-        if (!decodedToken) throw new Error('토큰이 정상적이지 않습니다.');
+        let decodedToken;
+        try {
+            decodedToken = jwt.verify(token, SECRET_CODE);
+        } catch (error) {
+            throw new Error('유효하지 않거나 만료된 토큰입니다.');
+        }
 
         const userID = decodedToken.userID;
+        if (!userID) {
+            throw new Error('토큰에서 사용자 ID를 찾을 수 없습니다.');
+        }
 
         const user = await prisma.users.findFirst({
             where: { userID: userID },
@@ -27,7 +33,6 @@ export default async function (req, res, next) {
         if (!user) throw new Error('토큰 사용자가 존재하지 않습니다.');
 
         req.user = user;
-
         next();
     } catch (error) {
         return res.status(400).json({ message: error.message });
